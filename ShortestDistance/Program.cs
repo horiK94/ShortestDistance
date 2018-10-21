bddb123c9cb75a8a915dd336967e2b0f80a73cec
hamity <hazemi94basic@gmail.com>
2018-10-21 15:07:26 +0900

    プロジェクトの追加

---
+++ ShortestDistance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShortestDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
